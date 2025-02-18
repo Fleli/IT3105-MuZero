1846ddf2690ed422278a8a1ee9d6ca4da7704238
Fleli <frederikedvardsen@gmail.com>
2025-02-18 15:28:27 +0100

    Start MCTS

--- MCTS/MCTS.py
+++ MCTS/MCTS.py
@@ -1,16 +1,30 @@
 
+from NeuralNetwork.NeuralNetwork import *
 
+type ConcreteGameState = list[int]
 
 class MCTS:
     
-    
+    tree_policy: NeuralNetwork
+    default_policy: NeuralNetwork
     
     def __init__(self):
         pass
     
-    def rollout(self):
+    
+    # Do a Monte Carlo Tree Search
+    # - input: A list of the (q+1) last concrete game states s_(k-q), ..., s_(k)
+    # - output: The concrete move that is (hopefully) optimal
+    def search( concrete_game_states: list[ConcreteGameState] ):
+        
+        
+        
+        return None  # TODO: Return 
+    
+    
+    def _rollout(self):
         pass
     
-    def backpropagate(self):
+    def _backpropagate(self):
         pass
     
