513a46514e055cbd48db99ec7907d7ebae5e0bb3
Torgeir Lysen <32740390+torgeirlysen@users.noreply.github.com>
2025-04-03 12:17:54 +0200

    change: random weight generation

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,6 +1,7 @@
-import jax
 import jax.numpy as jnp
-import jax.random as random
+from jax import random
+import jax.nn as nn
+
 from NeuralNetwork.NeuralNetworkLayer import NeuralNetworkLayer
 
 key = random.PRNGKey(0)
@@ -19,35 +20,45 @@ class NeuralNetwork:
         activation_function = config['activation_function']
         self.k = config['k']
         self.learning_rate = config['learning_rate']
+
+        key = config.get('key', random.PRNGKey(0)) 
+        glorot = nn.initializers.glorot_normal()
+        bias_init = nn.initializers.normal(stddev=0.01)
+
+        key, input_key, *hidden_keys, output_key = random.split(key, len(layers) + 2)
+        
         self.input_layer = NeuralNetworkLayer(
             n_neurons=layers[0],
             activation_function=activation_function,
             params={
-                "weights": random.normal(key, (layers[0], input_dim)) * 0.1,
-                "bias": random.normal(key, (layers[0],)) * 0.00001
+                "weights": glorot(input_key, (layers[0], input_dim)),
+                "bias": bias_init(input_key, (layers[0],))
             }
         )
-        self.hidden_layers = [
-            NeuralNetworkLayer(
-                n_neurons=n,
-                activation_function=activation_function,
-                params={
-                    "weights": random.normal(key, (n, in_dim)) * 0.1,
-                    "hidden_weights": random.normal(key, (n, n)) * 0.1,
-                    "bias": random.normal(key, (n,)) * 0.00000
-                }
-            )
-            for n, in_dim in zip(layers[1:], layers[:-1])
-        ]
 
+        self.hidden_layers = []
+        for i, (n, in_dim) in enumerate(zip(layers[1:], layers[:-1])):
+            key = hidden_keys[i]
+            self.hidden_layers.append(
+                NeuralNetworkLayer(
+                    n_neurons=n,
+                    activation_function=activation_function,
+                    params={
+                        "weights": glorot(key, (n, in_dim)),
+                        "hidden_weights": glorot(key, (n, n)), 
+                        "bias": bias_init(key, (n,))
+                    }
+                )
+            )
+
+        # Output layer
         self.output_layer = NeuralNetworkLayer(
             n_neurons=output_dim,
             activation_function="identity",
             params={
-                "weights": random.normal(key, (output_dim, layers[-1])) * 0.1,
-                # typically no recurrent connection in output.
-                "hidden_weights": jnp.zeros((output_dim,)),
-                "bias": random.normal(key, (output_dim,)) * 0.00001
+                "weights": glorot(output_key, (output_dim, layers[-1])),
+                "hidden_weights": jnp.zeros((output_dim,)), 
+                "bias": bias_init(output_key, (output_dim,))
             }
         )
         self.layer_parameters = [self.input_layer.parameters] + \
@@ -61,12 +72,10 @@ class NeuralNetwork:
         if params is None:
             params = self.layer_parameters
         current_input = self.input_layer.compute_output(input, params[0])
-        hidden_states = []
 
         for i, layer in enumerate(self.hidden_layers):
             new_state = layer.compute_output(
                 current_input, params[i + 1])
-            hidden_states.append(new_state)
             current_input = new_state
 
         output = self.output_layer.compute_output(
